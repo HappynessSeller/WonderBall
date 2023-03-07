524106b822a16bff7099de2250b13795664151d2
HappynessSeller <denis.troinich.89@gmail.com>
2023-03-07 23:10:41 +0200

    Moved SpawnPoint.cs to Editor folder

--- Assets/WonderBall/Scripts/Editor/SpawnPoint.cs
+++ Assets/WonderBall/Scripts/Editor/SpawnPoint.cs
@@ -4,9 +4,11 @@ namespace Wonderseat
 {
     public class SpawnPoint : MonoBehaviour
     {
+    #if UNITY_EDITOR
         private void OnDrawGizmos()
         {
             Gizmos.DrawIcon(transform.position + new Vector3(0, 0.5f, 0), "SpawnPoint.png", true);
         }
+    #endif
     }
 }
